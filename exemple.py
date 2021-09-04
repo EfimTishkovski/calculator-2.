b70aad5512ca6e2704fef8153f4ded5e30dc2e44
efim <schmurge22@mail.ru>
2021-09-04 21:49:20 +0300

    написана основная функция математика

--- exemple.py
+++ exemple.py
@@ -1,28 +1,5 @@
 # Разработка логики калькулятора
-# На вход подаётся проверенная строка!
-operation = input()  # Входная строка с мат. операциями и числами
 
-# Разбивка исходной строки по числам и выделение символов операций
-mass_element = []                                       # Массив разделённых элементов
-operators = ['*', '/', '+', '-', '%','(',')','^']
-number = ''                                             # Переменная для формирования числа
-for i in range(len(operation)):
-    if operation[i].isdigit() or operation[i] == '.':   # Если элемент число целое или типа float
-        number += operation[i]                          # Пока попадаются не символы из массива операторв они формируются в число
-    elif operation[i] in operators and number != '':    # Если элемент символ из массива операторов и за оператором идёт число
-        mass_element.append(float(number))              # Добавление числа (тип float) в массив
-        mass_element.append(operation[i])               # Добавление символа в массив
-        number = ''                                     # "Обнуление" переменной для формирования числа
-    elif operation[i] in operators and number == '':    # Если элемент символ из массива операторов и за оператром идёт другой оператор (иначе был лишний пустой элемент)
-        mass_element.append(operation[i])               # Добавление символа в массив
-        number = ''                                     # "Обнуление" переменной для формирования числа
-else:
-    if number.isdigit():                                # Добавление последнего элемента, с этим были проблемы
-        mass_element.append(float(number))              # Если число, то float и в массив
-    elif number in operators and number != '':          # Если оператор, то соответствие с масс операторов и не пустой
-        mass_element.append(number)
-# После цикла имеем массив отдельных элементов
-#print(mass_element)
 # Функция элементарных мат. вычислений
 def elementary_operations(a,b,operator):
     if operator == '+':
@@ -51,9 +28,9 @@ def found_and_run(input_mass, operator):
     while flag:
         # Поиск
         for i in range(1, len(mass)):
-            #print(mass[i], i)
-            if mass[i] == operator:                                               # Если совпало, высичиляем
-                #print(mass[i])
+            print(mass[i], i)
+            if mass[i] in operator:                                               # Если совпало, высичиляем
+                print(mass[i])
                 mass[i] = elementary_operations(mass[i - 1],mass[i + 1], mass[i]) # Меняем оператор на результат
                 del mass[i + 1]                                                   # Удаляем исходные цифры из массива
                 del mass[i - 1]                                                   # Удаляем исходные цифры из массива
@@ -88,56 +65,68 @@ def run_hooks(input_mass):
 
     for k in range(a + 1, b):
         mass_in_hooks.append(input_mass[k])
-    #print(mass_in_hooks)
+    print(mass_in_hooks)
     # Поиск окночен в mass_in_hooks выражение в скобках
-    # Вычисления
+    # Вычисления, всегда 2 числа (x  y)
     for element in priority_operators_1:
         result = found_and_run(mass_in_hooks,element)     # Вычисления по операторам с приоритетом 1
-    if len(result) <= 1:                                  # Проверка на завершение вычислений, если число одно, то стоп
-        return result
 
     for element in priority_operators_3:
         result = found_and_run(result, element)           # Вычисления по операторам с приоритетом 3
-    if len(result) <= 1:
-        return result
 
     for element in priority_operators_4:
         result = found_and_run(result, element)           # Вычисления по операторам с приоритетом 4
-    if len(result) <= 1:
-        return result
 
-print(mass_element)
-print(run_hooks(mass_element))
+    mass[a] = result                                      # Замена выражения в скобках результатом
+    #print(a, b)
+    for i in range(b, a, -1):
+        del mass[i]
+    return mass                                           # Возврат преобразованного массива
+
+# Основная функция для вычислений
+# На входе строка типа: 1+23-8*(1+3)
+def matematika(operation):
+    # На вход подаётся проверенная строка!
+
+    # Разбивка исходной строки по числам и выделение символов операций
+    mass_element = []  # Массив разделённых элементов
+    operators = ['*', '/', '+', '-', '%', '(', ')', '^']
+    number = ''  # Переменная для формирования числа
+    for i in range(len(operation)):
+        if operation[i].isdigit() or operation[i] == '.':  # Если элемент число целое или типа float
+            number += operation[i]  # Пока попадаются не символы из массива операторв они формируются в число
+        elif operation[i] in operators and number != '':  # Если элемент символ из массива операторов и за оператором идёт число
+            mass_element.append(float(number))  # Добавление числа (тип float) в массив
+            mass_element.append(operation[i])  # Добавление символа в массив
+            number = ''  # "Обнуление" переменной для формирования числа
+        elif operation[i] in operators and number == '':  # Если элемент символ из массива операторов и за оператром ещё оператор (иначе был лишний пустой элемент)
+            mass_element.append(operation[i])  # Добавление символа в массив
+            number = ''  # "Обнуление" переменной для формирования числа
+    else:
+        if number.isdigit():  # Добавление последнего элемента, с этим были проблемы
+            mass_element.append(float(number))  # Если число, то float и в массив
+        elif number in operators and number != '':  # Если оператор, то соответствие с масс операторов и не пустой
+            mass_element.append(number)
+    # После цикла имеем массив отдельных элементов
+    print(mass_element)
+    # Основной блок вычислений
+    # mass_element   массив до вычислений
+    # mass_after_run массив после вычислений
+    mass_after_run = []
+    while True:
+        mass_after_run.extend(found_and_run(mass_element, priority_operators_1))    # Поиск операторов с приоритетом 1
+        print(found_and_run(mass_element, priority_operators_1))
+        if len(mass_after_run) == len(mass_element):                                # Проверка на наличие операторов
+            #print(mass_element)
+            mass_element.clear()                                   # Очистка входного списка
+            mass_element.extend(mass_after_run)                    # Запись туда массива после вычислений
+            break                                                  # Остановка цикла если оператров больше нет
+        else:
+            mass_element.clear()
+            mass_element.extend(mass_after_run)
+        if len(mass_element) == 1:                                 # Проверка на заверщение работы функции
+            return mass_element                                 # Если там одно число, его и выводим
+
+    #print(run_hooks(mass_element))
 
 
-
-
-
-
-
-
-"""
-for element in priority_operators_1:
-    a = found_and_run(mass_element, element)
-print(a)
-"""
-
-
-
-
-
-
-
-
-
-#i = 5
-#print(elementary_operations(mass_element[i - 1],mass_element[i + 1], mass_element[i]))
-#result = elementary_operations(mass_element[i - 1],mass_element[i + 1], mass_element[i])
-#mass_element[i] = result
-#del mass_element[i + 1]
-#del mass_element[i - 1]
-#print(mass_element)
-
-# Цикл выделения чисел и операций
-# Вычисления проводятся соглано приорететам операций
-# Цикл заканчивается когда в массиве остаётся одно число
