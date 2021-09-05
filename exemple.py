6a2eeb88ecb6daf6a5cfa15c4be87cb7f6979ce6
efim <schmurge22@mail.ru>
2021-09-05 15:08:34 +0300

    Исправлены ошибки в функции вычисления в скобках. Эта функция тестируется. Тестируется поведение математики при её вызове из вне. Дорабатывается логика метематики

--- exemple.py
+++ exemple.py
@@ -23,7 +23,8 @@ priority_operators_4 = ['+','-']  # Операторы с приорететом
 
 # Функция поиска операторов и выполнение вычислений
 def found_and_run(input_mass, operator):
-    mass = input_mass
+    mass = []                                # Создание буферного массива
+    mass.extend(input_mass)                  # Создание копии входного массива
     flag = True
     while flag:
         # Поиск
@@ -43,11 +44,12 @@ def found_and_run(input_mass, operator):
 # Проверка строки не производится!
 # Строка должна быть проверена ещё на входе!
 def run_hooks(input_mass):
-    mass = input_mass
-    mass_in_hooks = []                              # массив для выражения в скобках
+    mass = []                                  # Создание буферного массива
+    mass.extend(input_mass)                    # Создание копии входного массива
+    mass_in_hooks = []                         # массив для выражения в скобках
     flag = True
-    a = -1                                          # Начальное значение, если неизменно, то скобок не найдено
-    b = -1                                          # Начальное значение, если неизменно, то скобок не найдено
+    a = -1                                     # Начальное значение, если неизменно, то скобок не найдено
+    b = -1                                     # Начальное значение, если неизменно, то скобок не найдено
     while flag:
         # Поиск
         for i in range(len(mass)):
@@ -56,31 +58,54 @@ def run_hooks(input_mass):
                 for j in range(i, len(mass) + 1):    # поиск индекса второй скобки
                     if mass[j] == ')':
                         b = j
-                        flag = False
+                        #flag = False
                         break
                     if mass[j] == '(':               # Если найдена новая открытая скобка, ей присваевается новый индекс
                         a = j
         if a < 0:                                    # Если не найдено открытой скобки
-            return input_mass
+            return mass                         # Возврат массива, если скобок не было, то вернётся исходный
 
-    for k in range(a + 1, b):
-        mass_in_hooks.append(input_mass[k])
-    print(mass_in_hooks)
-    # Поиск окночен в mass_in_hooks выражение в скобках
-    # Вычисления, всегда 2 числа (x  y)
-    for element in priority_operators_1:
-        result = found_and_run(mass_in_hooks,element)     # Вычисления по операторам с приоритетом 1
+        for k in range(a + 1, b):
+            mass_in_hooks.append(input_mass[k])
+        #print(mass_in_hooks)
+        # Поиск окночен в mass_in_hooks выражение в скобках
+        # Вычисления, всегда 2 числа (x  y)
+        result = []
+        result.extend(found_and_run(mass_in_hooks,priority_operators_1)) # Вычисления по операторам с приоритетом 1
+        #print(result)
+        if len(result) == len(mass_in_hooks):
+            result.clear()
+        else:
+            mass_in_hooks.clear()
+            mass_in_hooks.extend(result)
+            result.clear()
 
-    for element in priority_operators_3:
-        result = found_and_run(result, element)           # Вычисления по операторам с приоритетом 3
+        result.extend(found_and_run(mass_in_hooks, priority_operators_3))  # Вычисления по операторам с приоритетом 3
+        #print(result)
+        if len(result) == len(mass_in_hooks):
+            result.clear()
+        else:
+            mass_in_hooks.clear()
+            mass_in_hooks.extend(result)
+            result.clear()
 
-    for element in priority_operators_4:
-        result = found_and_run(result, element)           # Вычисления по операторам с приоритетом 4
+        result.extend(found_and_run(mass_in_hooks, priority_operators_4))  # Вычисления по операторам с приоритетом 4
+        #print(result)
+        if len(result) == len(mass_in_hooks):
+            result.clear()
+        else:
+            mass_in_hooks.clear()
+            mass_in_hooks.extend(result)
+            result.clear()
 
-    mass[a] = result                                      # Замена выражения в скобках результатом
-    #print(a, b)
-    for i in range(b, a, -1):
-        del mass[i]
+        mass[a] = mass_in_hooks[0]              # Замена первой скобки на результат
+        #print(a, b)
+        for i in range(b, a, -1):
+            del mass[i]                                      # Удаление из массива выражения в скобках
+        #print(mass)
+        a = -1      # Сброс переменных
+        b = -1
+        mass_in_hooks.clear()
     return mass                                           # Возврат преобразованного массива
 
 # Основная функция для вычислений
@@ -93,40 +118,46 @@ def matematika(operation):
     operators = ['*', '/', '+', '-', '%', '(', ')', '^']
     number = ''  # Переменная для формирования числа
     for i in range(len(operation)):
-        if operation[i].isdigit() or operation[i] == '.':  # Если элемент число целое или типа float
-            number += operation[i]  # Пока попадаются не символы из массива операторв они формируются в число
-        elif operation[i] in operators and number != '':  # Если элемент символ из массива операторов и за оператором идёт число
-            mass_element.append(float(number))  # Добавление числа (тип float) в массив
-            mass_element.append(operation[i])  # Добавление символа в массив
-            number = ''  # "Обнуление" переменной для формирования числа
-        elif operation[i] in operators and number == '':  # Если элемент символ из массива операторов и за оператром ещё оператор (иначе был лишний пустой элемент)
-            mass_element.append(operation[i])  # Добавление символа в массив
-            number = ''  # "Обнуление" переменной для формирования числа
+        if operation[i].isdigit() or operation[i] == '.':   # Если элемент число целое или типа float
+            number += operation[i]                          # Пока попадаются не символы-цифры они формируются в число
+        elif operation[i] in operators and number != '':    # Если элемент - оператор и за оператором идёт число
+            mass_element.append(float(number))              # Добавление числа (тип float) в массив
+            mass_element.append(operation[i])               # Добавление символа в массив
+            number = ''                                     # "Обнуление" переменной для формирования числа
+        elif operation[i] in operators and number == '':    # Если элемент - оператор и за ним ещё оператор (иначе был лишний пустой элемент)
+            mass_element.append(operation[i])               # Добавление символа в массив
+            number = ''                                     # "Обнуление" переменной для формирования числа
     else:
         if number.isdigit():  # Добавление последнего элемента, с этим были проблемы
             mass_element.append(float(number))  # Если число, то float и в массив
         elif number in operators and number != '':  # Если оператор, то соответствие с масс операторов и не пустой
             mass_element.append(number)
     # После цикла имеем массив отдельных элементов
-    #print(mass_element)
+    print(mass_element)
+
+
     # Основной блок вычислений
     # mass_element   массив до вычислений
     # mass_after_run массив после вычислений
-    mass_after_run = []
-    while True:
-        mass_after_run.extend(found_and_run(mass_element, priority_operators_1))    # Поиск операторов с приоритетом 1
-        #print(found_and_run(mass_element, priority_operators_1))
-        if len(mass_after_run) == len(mass_element):                                # Проверка на наличие операторов
-            mass_element.clear()                                   # Очистка входного списка
-            mass_element.extend(mass_after_run)                    # Запись туда массива после вычислений
-            return mass_element
-            break                                                  # Остановка цикла если оператров больше нет
-        else:
-            mass_element.clear()
-            mass_element.extend(mass_after_run)
-        if len(mass_element) == 1:                                 # Проверка на заверщение работы функции
-            return mass_element                                 # Если там одно число, его и выводим
+    mass_after_run = []  # Буферный список
+    mass_after_run.extend(found_and_run(mass_element, priority_operators_1))    # Поиск ВСЕХ операторов с приоритетом 1
+    mass_element.clear()                    # Очистка входного списка
+    mass_element.extend(mass_after_run)     # Перезапись входного списка
+    mass_after_run.clear()                  # Очистка буферного списка
+    if len(mass_element) == 1:              # Проверка на окончание работы
+        mass_after_run.clear()              # Очистка буферного списка
+        return mass_element[0]              # Если там одно число, его и выводим
 
-    #print(run_hooks(mass_element))
+    mass_after_run.extend(found_and_run(mass_element, priority_operators_3))  # Поиск ВСЕХ операторов с приоритетом 3
+    mass_element.clear()
+    mass_element.extend(mass_after_run)
+    mass_after_run.clear()
+    if len(mass_element) == 1:
+        mass_after_run.clear()
+        return mass_element[0]
+
+    return mass_element
+    
+
 
 
